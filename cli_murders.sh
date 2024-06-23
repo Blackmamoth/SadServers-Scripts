707728cda6837c9808e22c7d1769695b393cacca
blackmamoth <blackmamoth83@gmail.com>
2024-06-23 20:08:42 +0530

    Added solution for "The Command Line Murders"

---
+++ cli_murders.sh
@@ -0,0 +1,9 @@
+cat crimescene | grep CLUE
+
+cat people | grep Annabel
+
+cat streets/Buckingham_Place
+
+cat interviews/interview-699607
+
+cat vehicles | grep "L337" -C 4 | grep Blue -C 4 | grep Honda -C 4
